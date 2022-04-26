ded5519d0ec2008f3a6ec6a5d0cbd5eeb5e46bca
edu <edu@mail.pl>
2022-04-26 16:23:45 +0200

    servlety zad1

---
+++ src/main/java/pl/coderslab/FirstServlet.java
@@ -0,0 +1,16 @@
+package pl.coderslab;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("hello first servlet");
+    }
+}
